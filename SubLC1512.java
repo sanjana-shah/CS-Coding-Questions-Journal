cdf7fc9df42488b80e7e44a6d05a980598e14efb
Sanjana <53399985+sanjana-shah@users.noreply.github.com>
2020-12-20 16:31:14 +0530

    Create SubLC1512.java
    This was such a neat solution put by someone on the discussion page

---
+++ SubLC1512.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int res=0, count[]=new int[101];
+        for(int i:nums)
+            res+=count[i]++;
+        return res;
+    }
+}
